fac3a3c0bf516b37afda3767d479c45e63798112
HP <HP@DESKTOP-35CHJSD.mshome.net>
2022-11-30 23:57:32 +0200

    Accounting

---
+++ src/main/java/telran/java45/accounting/service/UserAccountServiceImpl.java
@@ -0,0 +1,86 @@
+package telran.java45.accounting.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import telran.java45.accounting.dao.UserAccountRepository;
+import telran.java45.accounting.dto.RolesResponseDto;
+import telran.java45.accounting.dto.UserAccountResponseDto;
+import telran.java45.accounting.dto.UserRegisterDto;
+import telran.java45.accounting.dto.UserUpdateDto;
+import telran.java45.accounting.dto.exceptions.UserExistsException;
+import telran.java45.accounting.dto.exceptions.UserNotFoundException;
+import telran.java45.accounting.model.UserAccount;
+
+@Service
+@RequiredArgsConstructor
+public class UserAccountServiceImpl implements UserAccountService {
+
+	final UserAccountRepository repository;
+	final ModelMapper modelMapper;
+	
+	@Override
+	public UserAccountResponseDto addUser(UserRegisterDto userRegisterDto) {
+		if(repository.existsById(userRegisterDto.getLogin())) {
+			throw new UserExistsException(userRegisterDto.getLogin());
+		}
+		UserAccount userAccount = modelMapper.map(userRegisterDto, UserAccount.class);
+		repository.save(userAccount);
+		return modelMapper.map(userAccount, UserAccountResponseDto.class);
+	}
+
+	@Override
+	public UserAccountResponseDto getUser(String login) {
+		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException(login));
+		return modelMapper.map(userAccount, UserAccountResponseDto.class);
+	}
+
+	@Override
+	public UserAccountResponseDto removeUser(String login) {
+		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException(login));
+		repository.delete(userAccount);
+		return modelMapper.map(userAccount, UserAccountResponseDto.class);
+
+	}
+
+	@Override
+	public UserAccountResponseDto editUser(String login, UserUpdateDto userUpdateDto) {
+		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException(login));
+		if(userUpdateDto.getFirstName()!=null) {
+			userAccount.setFirstName(userUpdateDto.getFirstName());
+		}
+		if(userUpdateDto.getLastName()!=null) {
+			userAccount.setLastName(userUpdateDto.getLastName());
+		}
+		repository.save(userAccount);
+		return modelMapper.map(userAccount, UserAccountResponseDto.class);
+	}
+
+	@Override
+	public RolesResponseDto changeRolesList(String login, String role, boolean isAddRole) {
+		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException(login));
+		if(isAddRole) {
+			userAccount.addRole(role);
+		} else {
+			if(userAccount.getRoles().contains(role)) {
+				userAccount.removeRole(role);
+			}
+			
+		}
+		repository.save(userAccount);
+		return modelMapper.map(userAccount, RolesResponseDto.class);
+	}
+
+	@Override
+	public void changePassword(String login, String newPassword) {
+		UserAccount userAccount = repository.findById(login).orElseThrow(() -> new UserNotFoundException(login));
+		if(newPassword!=null) {
+			userAccount.setPassword(newPassword);
+		}
+		repository.save(userAccount);
+
+	}
+
+}
